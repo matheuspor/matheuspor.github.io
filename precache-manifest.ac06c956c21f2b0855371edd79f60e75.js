cd8dc6ba77d1a473cfa7e1bad0f81d1b10cff398
Matheus <matheus.pormanhani@gmail.com>
2021-09-05 19:19:01 -0300

    Updates

---
+++ precache-manifest.ac06c956c21f2b0855371edd79f60e75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41dafb02b3af0ad5c67a5a151862bb15",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "b0b16122424f78df86f3",
+    "url": "/trybewallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "48de166202430da06607",
+    "url": "/trybewallet/static/js/2.19513e58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.19513e58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0b16122424f78df86f3",
+    "url": "/trybewallet/static/js/main.fa351b1f.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
